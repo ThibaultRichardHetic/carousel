142a8a8c71a3397fd4fa9fc4f03b8723dd873aae
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-11-26 12:59:31 +0100

    :sparkles: set carousel

--- src-carousel/app.js
+++ src-carousel/app.js
@@ -3,6 +3,75 @@ class HeticCarousel
     constructor($container)
     {
         this.$container = $container
-        console.log('new carousel')
+        
+        this.setSlides()
+        this.setSiblings()
+    }
+
+    setSlides()
+    {
+        this.slides = {}
+        this.slides.index = 0
+        this.slides.$items = this.$container.querySelectorAll('.slide')
+    }
+
+    setSiblings()
+    {
+        this.siblings = {}
+        this.siblings.active = !!this.$container.dataset.siblings
+
+        if(!this.siblings.active)
+        {
+            return
+        }
+
+        // Create DOM
+        this.siblings.$previous = document.createElement('button')
+        this.siblings.$previous.classList.add('sibling', 'previous')
+        this.$container.appendChild(this.siblings.$previous)
+
+        this.siblings.$next = document.createElement('button')
+        this.siblings.$next.classList.add('sibling', 'next')
+        this.$container.appendChild(this.siblings.$next)
+
+        // Listen click events
+        this.siblings.$previous.addEventListener('click', () =>
+        {
+            this.previous()
+        })
+        this.siblings.$next.addEventListener('click', () =>
+        {
+            this.next()
+        })
+    }
+
+    previous()
+    {
+        let index = this.slides.index - 1
+
+        if(index < 0)
+        {
+            index = this.slides.$items.length - 1
+        }
+
+        this.goTo(index)
+    }
+
+    next()
+    {
+        let index = this.slides.index + 1
+
+        if(index > this.slides.$items.length - 1)
+        {
+            index = 0
+        }
+
+        this.goTo(index)
+    }
+
+    goTo(_index)
+    {
+        console.log(_index)
+        this.slides.index = _index
     }
 }
